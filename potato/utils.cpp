9e05fd0126cffe32de858d892ab7eee94a77aad1
Lo-X <loic.boutter@gmail.com>
2014-01-14 21:42:47 +0100

    Added centerOrigin() for animations

--- potato/utils.cpp
+++ potato/utils.cpp
@@ -12,8 +12,8 @@ void centerOrigin(sf::Text& text)
     text.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
 }
 
-/*void centerOrigin(Animation& animation)
+void centerOrigin(Animation& animation)
 {
     sf::FloatRect bounds = animation.getLocalBounds();
     animation.setOrigin(std::floor(bounds.width / 2.f), std::floor(bounds.height / 2.f));
-}*/
+}
--- potato/utils.h
+++ potato/utils.h
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <sstream>
+#include "resources/animation.h"
 
 // Since std::to_string doesn't work on MinGW we have to implement
 // our own to support all platforms.
@@ -12,7 +13,7 @@ std::string toString(const T& value);
 // Helpers to set the origin of sprites/text/... at center
 void centerOrigin(sf::Sprite& sprite);
 void centerOrigin(sf::Text& text);
-//void centerOrigin(Animation& animation);
+void centerOrigin(Animation& animation);
 
 #include "utils.inl"
 
