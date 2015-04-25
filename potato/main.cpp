f8ee6fc235dcb4f5ef651df241f9b6a4f1e0f4c7
Lo-X <loic.boutter@gmail.com>
2015-04-25 16:08:35 +0200

    Adding a default main.cpp file

---
+++ potato/main.cpp
@@ -0,0 +1,18 @@
+#include "application.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const unsigned int width = 1280, height = width * 9/16;
+    try {
+        Application game(width, height, "Potato Framework Application");
+        game.run();
+    } catch(std::exception& e) {
+        std::cerr << "Game ended unexpectedely : " << std::endl;
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
+
